0a90af49749123719936c557b1faf2f80f8545f5
0xSnowmn <ya860374@gmail.com>
2023-10-10 05:26:47 +0200

    add some comments to make it more easy to understand

--- core/imageOptmize.go
+++ core/imageOptmize.go
@@ -16,14 +16,21 @@ import (
 	"github.com/h2non/bimg"
 )
 
+var folderName = "./screenshots/"
+var pngFile string
+var Screename string
+
+// Decode the base64 srtring sent from js callback to png file
 func DecodeImage(b64 string, domain string) {
+	// Create folder for screenshots and check if exist or not
+	createFolder(folderName)
 	reader := base64.NewDecoder(base64.StdEncoding, strings.NewReader(b64))
 	m, _, err := image.Decode(reader)
 	if err != nil {
 		log.Fatal(err)
 	}
 	pngFilename := domain + ".png"
-	f, err := os.Create("./screenshots/" + pngFilename)
+	f, err := os.Create(folderName + pngFilename)
 	if err != nil {
 		log.Fatal(err)
 		return
@@ -34,15 +41,19 @@ func DecodeImage(b64 string, domain string) {
 		log.Fatal(err)
 		return
 	}
-	ff, _ := os.Open("./screenshots/" + pngFilename)
+	ff, _ := os.Open(folderName + pngFilename)
 	buffer, err := io.ReadAll(ff)
 	if err != nil {
 		panic(err)
 	}
+	// Start the proccess of optmizing the png file to reduce the size
 	d, err := ProccessImage(buffer)
 	if err != nil {
 		panic(err)
 	}
+	// clean png file after finish the optmiztion
+	cleanAfterOptmize(folderName + pngFilename)
+
 	fmt.Println(d)
 }
 
@@ -59,9 +70,10 @@ func createFolder(dirname string) error {
 
 // The mime type of the image is changed, it is compressed and then saved in the specified folder.
 func ProccessImage(buffer []byte) (string, error) {
-	createFolder("screenshots")
+	fmt.Println(Screename)
 	currentTime := time.Now()
 
+	// create the uniqu id for the new screen filename
 	screenName := fmt.Sprintf("%d-%d-%d_%d:%d:%d", currentTime.Year(),
 		currentTime.Month(),
 		currentTime.Day(),
@@ -76,15 +88,26 @@ func ProccessImage(buffer []byte) (string, error) {
 		return filename, err
 	}
 
+	// convert the file and edit quality
 	processed, err := bimg.NewImage(converted).Process(bimg.Options{Quality: 90})
 	if err != nil {
 		return filename, err
 	}
 
-	writeError := bimg.Write(fmt.Sprintf("./screenshots/%s", filename), processed)
+	writeError := bimg.Write(folderName+filename, processed)
 	if writeError != nil {
 		return filename, writeError
 	}
 
 	return filename, nil
+}
+
+func cleanAfterOptmize(fileName string) {
+	_, err := os.Stat(fileName)
+	if !os.IsNotExist(err) {
+		err := os.Remove(fileName)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
 }
